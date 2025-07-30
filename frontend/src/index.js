6d418d964c30e6aa7489c08221336dc3f8b37e7c
Vineet <vineetkushwaha6325@gmail.com>
2025-07-30 19:38:20 +0530

    Feat: swithced to cra

---
+++ frontend/src/index.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import Login from './pages/login';
+
+import Navbar from './components/Navbar';
+
+import reportWebVitals from './reportWebVitals';
+import ThemeProvider from './context/ThemeProvider';
+import { Routes, Route, BrowserRouter } from 'react-router-dom';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <ThemeProvider>
+      <BrowserRouter>
+      <Navbar></Navbar>
+        <Routes><Route>{<App />}</Route></Routes>
+        <Routes><Route>{<Login />}</Route></Routes>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();

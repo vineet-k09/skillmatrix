e6682b12f04721eaa23c0ea2cc9bfa1cdcbb14b6
Shyamkrishna B Nair <shyamkrishnabnair@gmail.com>
2025-08-01 20:00:58 +0530

    Donnt mind the bg color

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -52,7 +52,7 @@ export default function Navbar(){
         //     </div>
         // </nav>
 
-        <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <nav class="navbar navbar-expand-lg ">
   <div class="container-fluid">
     <Link className="navbar-brand text-primary fs-1" to={'/'}>SkillMatriX</Link>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -71,10 +71,11 @@ export default function Navbar(){
         </li>
       </ul>
       <form class="d-flex" role="search">
-        <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-        <button class="btn btn-outline-success fs-5 mb-2" type="submit">Search</button>
+        <input class="form-control m-2" type="search" placeholder="Search" aria-label="Search"/>
+        <button class="btn btn-outline-success m-2" type="submit">Search</button>
       </form>
-      <Link className="nav-link" to={'/register'}><button className="btn btn-primary ms-2 fs-5 mb-2">Register</button></Link>
+      <Link className="nav-link" to={'/register'}><button className="btn btn-primary ms-2 fs-5 m-2">Register</button></Link>
+       <button className="nav-link btn btn-primary m-2" onClick={toggleTheme}>{theme === 'dark' ? <Sun /> : <Moon />}</button>
     </div>
   </div>
 </nav>
